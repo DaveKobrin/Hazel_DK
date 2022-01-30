ee38d7b7ea12b4ecc380550ac34deb5080111b24
Dave Kobrin <david_kobrin@yahoo.com>
2022-01-29 20:53:25 -0500

    Setup project with dll and and application entry point.

---
+++ Hazel_DK/src/Hazel/Application.cpp
@@ -0,0 +1,18 @@
+#include "Application.h"
+
+namespace Hazel {
+	Application::Application() {
+
+	}
+	Application::~Application() {
+
+	}
+
+	void Application::Run() {
+		long count = 0;
+		while (true)
+		{
+			count++;
+		}
+	}
+}
