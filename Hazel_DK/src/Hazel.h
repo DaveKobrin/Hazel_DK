6891b8f060fbe621caf8f81f8cc0a4ab32579b79
Dave Kobrin <david_kobrin@yahoo.com>
2022-02-26 21:55:58 -0500

    Abstracted keycodes from glfw to hazel

--- Hazel_DK/src/Hazel.h
+++ Hazel_DK/src/Hazel.h
@@ -6,6 +6,7 @@
 #include "Hazel/Layer.h"
 #include "Hazel/Log.h"
 #include "Hazel/ImGui/ImGuiLayer.h"
+#include "Hazel/Input.h"
 
 //--- Entry Point ---------------------
 #include "Hazel/EntryPoint.h"
--- Hazel_DK/src/Hazel/ImGui/ImGuiLayer.cpp
+++ Hazel_DK/src/Hazel/ImGui/ImGuiLayer.cpp
@@ -1,6 +1,7 @@
 #include "hzpch.h"
 #include "ImGuiLayer.h"
 #include "Hazel/Application.h"
+#include "Hazel/InputCodes.h"
 
 //temporary hack opengl rendering for imgui
 #include "Hazel/Platform/OpenGL/imgui_opengl_Renderer.h"
@@ -135,114 +136,113 @@ namespace Hazel
 	{
         switch (key)
         {
-        case GLFW_KEY_TAB:              return ImGuiKey_Tab;
-        case GLFW_KEY_LEFT:             return ImGuiKey_LeftArrow;
-        case GLFW_KEY_RIGHT:            return ImGuiKey_RightArrow;
-        case GLFW_KEY_UP:               return ImGuiKey_UpArrow;
-        case GLFW_KEY_DOWN:             return ImGuiKey_DownArrow;
-        case GLFW_KEY_PAGE_UP:          return ImGuiKey_PageUp;
-        case GLFW_KEY_PAGE_DOWN:        return ImGuiKey_PageDown;
-        case GLFW_KEY_HOME:             return ImGuiKey_Home;
-        case GLFW_KEY_END:              return ImGuiKey_End;
-        case GLFW_KEY_INSERT:           return ImGuiKey_Insert;
-        case GLFW_KEY_DELETE:           return ImGuiKey_Delete;
-        case GLFW_KEY_BACKSPACE:        return ImGuiKey_Backspace;
-        case GLFW_KEY_SPACE:            return ImGuiKey_Space;
-        case GLFW_KEY_ENTER:            return ImGuiKey_Enter;
-        case GLFW_KEY_ESCAPE:           return ImGuiKey_Escape;
-        case GLFW_KEY_APOSTROPHE:       return ImGuiKey_Apostrophe;
-        case GLFW_KEY_COMMA:            return ImGuiKey_Comma;
-        case GLFW_KEY_MINUS:            return ImGuiKey_Minus;
-        case GLFW_KEY_PERIOD:           return ImGuiKey_Period;
-        case GLFW_KEY_SLASH:            return ImGuiKey_Slash;
-        case GLFW_KEY_SEMICOLON:        return ImGuiKey_Semicolon;
-        case GLFW_KEY_EQUAL:            return ImGuiKey_Equal;
-        case GLFW_KEY_LEFT_BRACKET:     return ImGuiKey_LeftBracket;
-        case GLFW_KEY_BACKSLASH:        return ImGuiKey_Backslash;
-        case GLFW_KEY_RIGHT_BRACKET:    return ImGuiKey_RightBracket;
-        case GLFW_KEY_GRAVE_ACCENT:     return ImGuiKey_GraveAccent;
-        case GLFW_KEY_CAPS_LOCK:        return ImGuiKey_CapsLock;
-        case GLFW_KEY_SCROLL_LOCK:      return ImGuiKey_ScrollLock;
-        case GLFW_KEY_NUM_LOCK:         return ImGuiKey_NumLock;
-        case GLFW_KEY_PRINT_SCREEN:     return ImGuiKey_PrintScreen;
-        case GLFW_KEY_PAUSE:            return ImGuiKey_Pause;
-        case GLFW_KEY_KP_0:             return ImGuiKey_Keypad0;
-        case GLFW_KEY_KP_1:             return ImGuiKey_Keypad1;
-        case GLFW_KEY_KP_2:             return ImGuiKey_Keypad2;
-        case GLFW_KEY_KP_3:             return ImGuiKey_Keypad3;
-        case GLFW_KEY_KP_4:             return ImGuiKey_Keypad4;
-        case GLFW_KEY_KP_5:             return ImGuiKey_Keypad5;
-        case GLFW_KEY_KP_6:             return ImGuiKey_Keypad6;
-        case GLFW_KEY_KP_7:             return ImGuiKey_Keypad7;
-        case GLFW_KEY_KP_8:             return ImGuiKey_Keypad8;
-        case GLFW_KEY_KP_9:             return ImGuiKey_Keypad9;
-        case GLFW_KEY_KP_DECIMAL:       return ImGuiKey_KeypadDecimal;
-        case GLFW_KEY_KP_DIVIDE:        return ImGuiKey_KeypadDivide;
-        case GLFW_KEY_KP_MULTIPLY:      return ImGuiKey_KeypadMultiply;
-        case GLFW_KEY_KP_SUBTRACT:      return ImGuiKey_KeypadSubtract;
-        case GLFW_KEY_KP_ADD:           return ImGuiKey_KeypadAdd;
-        case GLFW_KEY_KP_ENTER:         return ImGuiKey_KeypadEnter;
-        case GLFW_KEY_KP_EQUAL:         return ImGuiKey_KeypadEqual;
-        case GLFW_KEY_LEFT_SHIFT:       return ImGuiKey_LeftShift;
-        case GLFW_KEY_LEFT_CONTROL:     return ImGuiKey_LeftCtrl;
-        case GLFW_KEY_LEFT_ALT:         return ImGuiKey_LeftAlt;
-        case GLFW_KEY_LEFT_SUPER:       return ImGuiKey_LeftSuper;
-        case GLFW_KEY_RIGHT_SHIFT:      return ImGuiKey_RightShift;
-        case GLFW_KEY_RIGHT_CONTROL:    return ImGuiKey_RightCtrl;
-        case GLFW_KEY_RIGHT_ALT:        return ImGuiKey_RightAlt;
-        case GLFW_KEY_RIGHT_SUPER:      return ImGuiKey_RightSuper;
-        case GLFW_KEY_MENU:             return ImGuiKey_Menu;
-        case GLFW_KEY_0:                return ImGuiKey_0;
-        case GLFW_KEY_1:                return ImGuiKey_1;
-        case GLFW_KEY_2:                return ImGuiKey_2;
-        case GLFW_KEY_3:                return ImGuiKey_3;
-        case GLFW_KEY_4:                return ImGuiKey_4;
-        case GLFW_KEY_5:                return ImGuiKey_5;
-        case GLFW_KEY_6:                return ImGuiKey_6;
-        case GLFW_KEY_7:                return ImGuiKey_7;
-        case GLFW_KEY_8:                return ImGuiKey_8;
-        case GLFW_KEY_9:                return ImGuiKey_9;
-        case GLFW_KEY_A:                return ImGuiKey_A;
-        case GLFW_KEY_B:                return ImGuiKey_B;
-        case GLFW_KEY_C:                return ImGuiKey_C;
-        case GLFW_KEY_D:                return ImGuiKey_D;
-        case GLFW_KEY_E:                return ImGuiKey_E;
-        case GLFW_KEY_F:                return ImGuiKey_F;
-        case GLFW_KEY_G:                return ImGuiKey_G;
-        case GLFW_KEY_H:                return ImGuiKey_H;
-        case GLFW_KEY_I:                return ImGuiKey_I;
-        case GLFW_KEY_J:                return ImGuiKey_J;
-        case GLFW_KEY_K:                return ImGuiKey_K;
-        case GLFW_KEY_L:                return ImGuiKey_L;
-        case GLFW_KEY_M:                return ImGuiKey_M;
-        case GLFW_KEY_N:                return ImGuiKey_N;
-        case GLFW_KEY_O:                return ImGuiKey_O;
-        case GLFW_KEY_P:                return ImGuiKey_P;
-        case GLFW_KEY_Q:                return ImGuiKey_Q;
-        case GLFW_KEY_R:                return ImGuiKey_R;
-        case GLFW_KEY_S:                return ImGuiKey_S;
-        case GLFW_KEY_T:                return ImGuiKey_T;
-        case GLFW_KEY_U:                return ImGuiKey_U;
-        case GLFW_KEY_V:                return ImGuiKey_V;
-        case GLFW_KEY_W:                return ImGuiKey_W;
-        case GLFW_KEY_X:                return ImGuiKey_X;
-        case GLFW_KEY_Y:                return ImGuiKey_Y;
-        case GLFW_KEY_Z:                return ImGuiKey_Z;
-        case GLFW_KEY_F1:               return ImGuiKey_F1;
-        case GLFW_KEY_F2:               return ImGuiKey_F2;
-        case GLFW_KEY_F3:               return ImGuiKey_F3;
-        case GLFW_KEY_F4:               return ImGuiKey_F4;
-        case GLFW_KEY_F5:               return ImGuiKey_F5;
-        case GLFW_KEY_F6:               return ImGuiKey_F6;
-        case GLFW_KEY_F7:               return ImGuiKey_F7;
-        case GLFW_KEY_F8:               return ImGuiKey_F8;
-        case GLFW_KEY_F9:               return ImGuiKey_F9;
-        case GLFW_KEY_F10:              return ImGuiKey_F10;
-        case GLFW_KEY_F11:              return ImGuiKey_F11;
-        case GLFW_KEY_F12:              return ImGuiKey_F12;
-        default:                        return ImGuiKey_None;
-        }
-        
+        case HZ_KEY_TAB:            return ImGuiKey_Tab;
+        case HZ_KEY_LEFT:           return ImGuiKey_LeftArrow;
+        case HZ_KEY_RIGHT:          return ImGuiKey_RightArrow;
+        case HZ_KEY_UP:             return ImGuiKey_UpArrow;
+        case HZ_KEY_DOWN:           return ImGuiKey_DownArrow;
+        case HZ_KEY_PAGE_UP:        return ImGuiKey_PageUp;
+        case HZ_KEY_PAGE_DOWN:      return ImGuiKey_PageDown;
+        case HZ_KEY_HOME:           return ImGuiKey_Home;
+        case HZ_KEY_END:            return ImGuiKey_End;
+        case HZ_KEY_INSERT:         return ImGuiKey_Insert;
+        case HZ_KEY_DELETE:         return ImGuiKey_Delete;
+        case HZ_KEY_BACKSPACE:      return ImGuiKey_Backspace;
+        case HZ_KEY_SPACE:          return ImGuiKey_Space;
+        case HZ_KEY_ENTER:          return ImGuiKey_Enter;
+        case HZ_KEY_ESCAPE:         return ImGuiKey_Escape;
+        case HZ_KEY_APOSTROPHE:     return ImGuiKey_Apostrophe;
+        case HZ_KEY_COMMA:          return ImGuiKey_Comma;
+        case HZ_KEY_MINUS:          return ImGuiKey_Minus;
+        case HZ_KEY_PERIOD:         return ImGuiKey_Period;
+        case HZ_KEY_SLASH:          return ImGuiKey_Slash;
+        case HZ_KEY_SEMICOLON:      return ImGuiKey_Semicolon;
+        case HZ_KEY_EQUAL:          return ImGuiKey_Equal;
+        case HZ_KEY_LEFT_BRACKET:   return ImGuiKey_LeftBracket;
+        case HZ_KEY_BACKSLASH:      return ImGuiKey_Backslash;
+        case HZ_KEY_RIGHT_BRACKET:  return ImGuiKey_RightBracket;
+        case HZ_KEY_GRAVE_ACCENT:   return ImGuiKey_GraveAccent;
+        case HZ_KEY_CAPS_LOCK:      return ImGuiKey_CapsLock;
+        case HZ_KEY_SCROLL_LOCK:    return ImGuiKey_ScrollLock;
+        case HZ_KEY_NUM_LOCK:       return ImGuiKey_NumLock;
+        case HZ_KEY_PRINT_SCREEN:   return ImGuiKey_PrintScreen;
+        case HZ_KEY_PAUSE:          return ImGuiKey_Pause;
+        case HZ_KEY_KP_0:           return ImGuiKey_Keypad0;
+        case HZ_KEY_KP_1:           return ImGuiKey_Keypad1;
+        case HZ_KEY_KP_2:           return ImGuiKey_Keypad2;
+        case HZ_KEY_KP_3:           return ImGuiKey_Keypad3;
+        case HZ_KEY_KP_4:           return ImGuiKey_Keypad4;
+        case HZ_KEY_KP_5:           return ImGuiKey_Keypad5;
+        case HZ_KEY_KP_6:           return ImGuiKey_Keypad6;
+        case HZ_KEY_KP_7:           return ImGuiKey_Keypad7;
+        case HZ_KEY_KP_8:           return ImGuiKey_Keypad8;
+        case HZ_KEY_KP_9:           return ImGuiKey_Keypad9;
+        case HZ_KEY_KP_DECIMAL:     return ImGuiKey_KeypadDecimal;
+        case HZ_KEY_KP_DIVIDE:      return ImGuiKey_KeypadDivide;
+        case HZ_KEY_KP_MULTIPLY:    return ImGuiKey_KeypadMultiply;
+        case HZ_KEY_KP_SUBTRACT:    return ImGuiKey_KeypadSubtract;
+        case HZ_KEY_KP_ADD:         return ImGuiKey_KeypadAdd;
+        case HZ_KEY_KP_ENTER:       return ImGuiKey_KeypadEnter;
+        case HZ_KEY_KP_EQUAL:       return ImGuiKey_KeypadEqual;
+        case HZ_KEY_LEFT_SHIFT:     return ImGuiKey_LeftShift;
+        case HZ_KEY_LEFT_CONTROL:   return ImGuiKey_LeftCtrl;
+        case HZ_KEY_LEFT_ALT:       return ImGuiKey_LeftAlt;
+        case HZ_KEY_LEFT_SUPER:     return ImGuiKey_LeftSuper;
+        case HZ_KEY_RIGHT_SHIFT:    return ImGuiKey_RightShift;
+        case HZ_KEY_RIGHT_CONTROL:  return ImGuiKey_RightCtrl;
+        case HZ_KEY_RIGHT_ALT:      return ImGuiKey_RightAlt;
+        case HZ_KEY_RIGHT_SUPER:    return ImGuiKey_RightSuper;
+        case HZ_KEY_MENU:           return ImGuiKey_Menu;
+        case HZ_KEY_0:              return ImGuiKey_0;
+        case HZ_KEY_1:              return ImGuiKey_1;
+        case HZ_KEY_2:              return ImGuiKey_2;
+        case HZ_KEY_3:              return ImGuiKey_3;
+        case HZ_KEY_4:              return ImGuiKey_4;
+        case HZ_KEY_5:              return ImGuiKey_5;
+        case HZ_KEY_6:              return ImGuiKey_6;
+        case HZ_KEY_7:              return ImGuiKey_7;
+        case HZ_KEY_8:              return ImGuiKey_8;
+        case HZ_KEY_9:              return ImGuiKey_9;
+        case HZ_KEY_A:              return ImGuiKey_A;
+        case HZ_KEY_B:              return ImGuiKey_B;
+        case HZ_KEY_C:              return ImGuiKey_C;
+        case HZ_KEY_D:              return ImGuiKey_D;
+        case HZ_KEY_E:              return ImGuiKey_E;
+        case HZ_KEY_F:              return ImGuiKey_F;
+        case HZ_KEY_G:              return ImGuiKey_G;
+        case HZ_KEY_H:              return ImGuiKey_H;
+        case HZ_KEY_I:              return ImGuiKey_I;
+        case HZ_KEY_J:              return ImGuiKey_J;
+        case HZ_KEY_K:              return ImGuiKey_K;
+        case HZ_KEY_L:              return ImGuiKey_L;
+        case HZ_KEY_M:              return ImGuiKey_M;
+        case HZ_KEY_N:              return ImGuiKey_N;
+        case HZ_KEY_O:              return ImGuiKey_O;
+        case HZ_KEY_P:              return ImGuiKey_P;
+        case HZ_KEY_Q:              return ImGuiKey_Q;
+        case HZ_KEY_R:              return ImGuiKey_R;
+        case HZ_KEY_S:              return ImGuiKey_S;
+        case HZ_KEY_T:              return ImGuiKey_T;
+        case HZ_KEY_U:              return ImGuiKey_U;
+        case HZ_KEY_V:              return ImGuiKey_V;
+        case HZ_KEY_W:              return ImGuiKey_W;
+        case HZ_KEY_X:              return ImGuiKey_X;
+        case HZ_KEY_Y:              return ImGuiKey_Y;
+        case HZ_KEY_Z:              return ImGuiKey_Z;
+        case HZ_KEY_F1:             return ImGuiKey_F1;
+        case HZ_KEY_F2:             return ImGuiKey_F2;
+        case HZ_KEY_F3:             return ImGuiKey_F3;
+        case HZ_KEY_F4:             return ImGuiKey_F4;
+        case HZ_KEY_F5:             return ImGuiKey_F5;
+        case HZ_KEY_F6:             return ImGuiKey_F6;
+        case HZ_KEY_F7:             return ImGuiKey_F7;
+        case HZ_KEY_F8:             return ImGuiKey_F8;
+        case HZ_KEY_F9:             return ImGuiKey_F9;
+        case HZ_KEY_F10:            return ImGuiKey_F10;
+        case HZ_KEY_F11:            return ImGuiKey_F11;
+        case HZ_KEY_F12:            return ImGuiKey_F12;
+        default:                    return ImGuiKey_None;
+        }        
 	}
 
     void ImGuiLayer::UpdateMods(const int key, const bool isPressed)
@@ -251,20 +251,20 @@ namespace Hazel
 
         switch (key)
         {
-        case GLFW_KEY_LEFT_CONTROL:
-        case GLFW_KEY_RIGHT_CONTROL:
+        case HZ_KEY_LEFT_CONTROL:
+        case HZ_KEY_RIGHT_CONTROL:
             io.AddKeyEvent(ImGuiKey_ModCtrl, isPressed);
             break;
-        case GLFW_KEY_LEFT_SHIFT:
-        case GLFW_KEY_RIGHT_SHIFT:
+        case HZ_KEY_LEFT_SHIFT:
+        case HZ_KEY_RIGHT_SHIFT:
             io.AddKeyEvent(ImGuiKey_ModShift, isPressed);
             break;
-        case GLFW_KEY_LEFT_ALT:
-        case GLFW_KEY_RIGHT_ALT:
+        case HZ_KEY_LEFT_ALT:
+        case HZ_KEY_RIGHT_ALT:
             io.AddKeyEvent(ImGuiKey_ModAlt, isPressed);
             break;
-        case GLFW_KEY_LEFT_SUPER:
-        case GLFW_KEY_RIGHT_SUPER:
+        case HZ_KEY_LEFT_SUPER:
+        case HZ_KEY_RIGHT_SUPER:
             io.AddKeyEvent(ImGuiKey_ModSuper, isPressed);
             break;
         }
